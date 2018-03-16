cf1054c3783995e8e4cb6c0bc61dccd76e14f6d0
Nilton Paiva <nfrpaiva@gmail.com>
2018-03-15 21:21:04 -0300

    Organizando o código

--- src/main/kotlin/com/nfrpaiva/taime/TaimeApplication.kt
+++ src/main/kotlin/com/nfrpaiva/taime/TaimeApplication.kt
@@ -1,10 +1,20 @@
 package com.nfrpaiva.taime
 
+import com.nfrpaiva.taime.dominio.Cliente
+import com.nfrpaiva.taime.dominio.ClienteRepository
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class TaimeApplication
+class TaimeApplication{
+    @Bean
+    fun init (clienteRepository: ClienteRepository) = CommandLineRunner {
+        clienteRepository.saveAll( mutableListOf(Cliente(nome="Nilton Paiva"),
+                Cliente(nome="Barbara Carolina")))
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<TaimeApplication>(*args)
