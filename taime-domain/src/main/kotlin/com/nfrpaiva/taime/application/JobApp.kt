ae6a622ce0974bdfdcac2998466be998af768344
Nilton Paiva <nfrpaiva@gmail.com>
2018-03-22 08:33:53 -0300

    Codando...

--- taime-domain/src/main/kotlin/com/nfrpaiva/taime/application/JobApp.kt
+++ taime-domain/src/main/kotlin/com/nfrpaiva/taime/application/JobApp.kt
@@ -7,10 +7,12 @@ import com.nfrpaiva.taime.dominio.ApontamentoRepository
 import com.nfrpaiva.taime.dominio.TrabalhoRepository
 import com.nfrpaiva.taime.exception.TaimeException
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Transactional
-sealed class JobApp {
+@Service
+class JobApp {
 
     @Autowired
     lateinit var apontamentoRepository: ApontamentoRepository
@@ -33,5 +35,3 @@ sealed class JobApp {
     }
 
 }
-
-
