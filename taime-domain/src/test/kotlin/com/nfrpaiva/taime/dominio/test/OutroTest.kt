aa1f4fb3a3b166b0409cd5ec4e76548dd3551b98
Nilton Paiva <nfrpaiva@gmail.com>
2018-05-20 20:31:01 -0300

    Codando..

---
+++ taime-domain/src/test/kotlin/com/nfrpaiva/taime/dominio/test/OutroTest.kt
@@ -0,0 +1,16 @@
+package com.nfrpaiva.taime.dominio.test
+
+import org.junit.Test
+
+class OutroTest {
+
+    @Test
+    fun primeiroTest() {
+        val pessoa = Pessoa(nome="Fernando")
+    }
+
+}
+
+class Pessoa(nome: String) {
+
+}
