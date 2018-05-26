be9f7039427ba9340d817a37d5dc81599fde8004
Nilton Paiva <nfrpaiva@gmail.com>
2018-05-26 14:04:10 -0300

    Codding

--- taime-domain/src/test/kotlin/com/nfrpaiva/taime/dominio/test/UmTesteComEntidades.kt
+++ taime-domain/src/test/kotlin/com/nfrpaiva/taime/dominio/test/UmTesteComEntidades.kt
@@ -2,13 +2,14 @@ package com.nfrpaiva.taime.dominio.test
 
 import com.fasterxml.jackson.annotation.JsonIgnore
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.databind.SerializationFeature.*
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 import org.springframework.test.context.junit4.SpringRunner
 import javax.persistence.*
 import javax.persistence.GenerationType.*
@@ -19,7 +20,9 @@ import javax.persistence.GenerationType.*
 class UmTesteComEntidades {
 
     @Autowired
-    lateinit var entityManager: EntityManager
+    lateinit var alunoRepository: AlunoRepository
+    @Autowired
+    lateinit var cursoRepository: CursoRepository
 
     @Test
     fun teste() {
@@ -27,24 +30,27 @@ class UmTesteComEntidades {
         val curso = Curso()
 
         for (i in 1..size) {
-            entityManager.persist(Aluno(curso = curso, nome = "Aluno $i"))
+            alunoRepository.save(Aluno(curso = curso, nome = "Aluno $i"))
         }
+        val listAlunos = alunoRepository.findAll()
+        val umCurso = cursoRepository.findById(curso.id!!).get()
 
-        val query = entityManager.createNamedQuery(Aluno.FIND_ALL, Aluno::class.java)
-        val result = query.resultList
-        assertThat(result).hasSize(size)
-        entityManager.clear()
-        val curso1 = entityManager.find(Curso::class.java, curso.id)
-        assertThat(curso1.alunos).hasSize(size)
+        assertThat(listAlunos).hasSize(size)
+        assertThat(umCurso.alunos).hasSize(size)
     }
 
 }
 
+@Repository
+interface CursoRepository : JpaRepository<Curso, Long>
+
+@Repository
+interface AlunoRepository : JpaRepository<Aluno, Long>
+
 @Entity
-@NamedQuery(name = Aluno.FIND_ALL, query = "select a from Aluno a")
 data class Aluno(
         @Id
-        @GeneratedValue(strategy = SEQUENCE,  generator = "SQ_ALUNO_GEN")
+        @GeneratedValue(strategy = SEQUENCE, generator = "SQ_ALUNO_GEN")
         @SequenceGenerator(name = "SQ_ALUNO_GEN", sequenceName = "SQ_ALUNO")
         var id: Long? = null,
         var nome: String,
@@ -54,10 +60,6 @@ data class Aluno(
         var curso: Curso
 
 ) {
-    companion object {
-        const val FIND_ALL = "Aluno.findAll"
-    }
-
     init {
         curso.alunos.add(this)
     }
@@ -71,7 +73,7 @@ data class Aluno(
 @Entity
 data class Curso(
         @Id
-        @GeneratedValue(strategy = SEQUENCE,  generator = "SQ_CURSO_GEN")
+        @GeneratedValue(strategy = SEQUENCE, generator = "SQ_CURSO_GEN")
         @SequenceGenerator(name = "SQ_CURSO_GEN", sequenceName = "SQ_CURSO")
         var id: Long? = null,
         @OneToMany(mappedBy = "curso")
