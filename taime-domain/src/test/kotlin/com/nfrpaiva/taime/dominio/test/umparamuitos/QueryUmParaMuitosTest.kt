939694735805d10dd55ff120353275650cd82b1a
Nilton Paiva <nfrpaiva@gmail.com>
2018-05-31 15:55:39 -0300

    Codando...

--- taime-domain/src/test/kotlin/com/nfrpaiva/taime/dominio/test/umparamuitos/QueryUmParaMuitosTest.kt
+++ taime-domain/src/test/kotlin/com/nfrpaiva/taime/dominio/test/umparamuitos/QueryUmParaMuitosTest.kt
@@ -21,7 +21,7 @@ import javax.persistence.*
 @DataJpaTest
 @Commit
 @ComponentScan(basePackageClasses = [QueryUmParaMuitosTest::class])
-@EnableJpaRepositories( basePackageClasses = [QueryUmParaMuitosTest::class])
+@EnableJpaRepositories(basePackageClasses = [QueryUmParaMuitosTest::class])
 @EntityScan(basePackageClasses = [QueryUmParaMuitosTest::class])
 class QueryUmParaMuitosTest {
 
@@ -51,7 +51,7 @@ class QueryUmParaMuitosTest {
     }
 
     @Test
-    fun `pai e filho com id 1`(){
+    fun `pai e filho com id 1`() {
         val list2 = em.createQuery("select p from Pai p join fetch p.filhos f where f.id =  1", Pai::class.java).resultList
         assertThat(list2).hasSize(1)
         assertThat(list2[0].filhos).hasSize(1)
@@ -59,7 +59,18 @@ class QueryUmParaMuitosTest {
     }
 
     @Test
-    fun `pai e  filho com id 2`(){
+    fun `pai e filho e filha com id 1`() {
+        val list2 = em.createQuery("select distinct p from Pai p " +
+                "join fetch p.filhos f join fetch p.filhas fa where f.id =  1 and fa.id <=2", Pai::class.java).resultList
+        assertThat(list2).hasSize(1)
+        assertThat(list2[0].filhos).hasSize(1)
+        assertThat(list2[0].filhas).hasSize(2)
+        assertThat(list2[0].filhos.iterator().next().id).isEqualTo(1)
+
+    }
+
+    @Test
+    fun `pai e  filho com id 2`() {
         val list3 = em.createQuery("select p from Pai p join fetch p.filhos f where f.id =  2", Pai::class.java).resultList
         assertThat(list3).hasSize(1)
         assertThat(list3[0].filhos).hasSize(1)
@@ -67,13 +78,13 @@ class QueryUmParaMuitosTest {
     }
 
     @Test
-    fun `pai e nenhum filho com id 3`(){
+    fun `pai e nenhum filho com id 3`() {
         val list4 = em.createQuery("select p from Pai p join fetch p.filhos f where f.id =  3", Pai::class.java).resultList
         assertThat(list4).hasSize(0)
     }
 
     @Test
-    fun `pai e todos os seus filhos`(){
+    fun `pai e todos os seus filhos`() {
         val list5 = em.createQuery("select p from Pai p join p.filhos f where f.id =  1", Pai::class.java).resultList
         assertThat(list5).hasSize(1)
         assertThat(list5[0].filhos).hasSize(2)
@@ -82,6 +93,7 @@ class QueryUmParaMuitosTest {
     private fun pai(): Pai {
         val pai = Pai(id = 1, nome = "Um Pai")
         pai.filhos = mutableSetOf(Filho(nome = "Filho 1", id = 1, pai = pai), Filho(nome = "Filho 2", id = 2, pai = pai))
+        pai.filhas = mutableSetOf(Filha(nome = "Filha 1", id = 1, pai = pai), Filha(nome = "Filha 2", id = 2, pai = pai), Filha(nome = "Filha 3", id = 3, pai = pai))
         return pai
     }
 
@@ -93,12 +105,16 @@ class QueryUmParaMuitosTest {
 
 }
 
-    @Entity
-    data class Pai(@Id var id: Long, var nome: String) {
-        @OneToMany(mappedBy = "pai", cascade = [CascadeType.ALL], orphanRemoval = true)
-        var filhos: MutableSet<Filho> = mutableSetOf()
+@Entity
+data class Pai(@Id var id: Long, var nome: String) {
+    @OneToMany(mappedBy = "pai", cascade = [CascadeType.ALL], orphanRemoval = true)
+    var filhos: MutableSet<Filho> = mutableSetOf()
+    @OneToMany(mappedBy = "pai", cascade = [CascadeType.ALL], orphanRemoval = true)
+    var filhas: MutableSet<Filha> = mutableSetOf()
+}
 
-    }
+@Entity
+data class Filho(@Id var id: Long, var nome: String, @ManyToOne var pai: Pai)
 
-    @Entity
-    data class Filho(@Id var id: Long, var nome: String, @ManyToOne var pai: Pai)
+@Entity
+data class Filha(@Id var id: Long, var nome: String, @ManyToOne var pai: Pai)
