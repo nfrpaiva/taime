ae844ebd3c0f20fc298a8b01df8f9ae0e9028611
Nilton Paiva <nfrpaiva@gmail.com>
2018-08-18 20:37:07 -0300

    alguns testes com logback

---
+++ taime-domain/src/test/kotlin/com/nfrpaiva/taime/dominio/test/logging/LoggingTest.kt
@@ -0,0 +1,25 @@
+package com.nfrpaiva.taime.dominio.test.logging
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class LoggingTest {
+
+    private val log: Logger = LoggerFactory.getLogger(this.javaClass)
+
+    @Test
+    fun logIt() {
+        log.error("Falhou")
+        log.info("Não falhou")
+//        val lc = LoggerFactory.getILoggerFactory() as LoggerContext
+//        StatusPrinter.print(lc)
+    }
+
+}
