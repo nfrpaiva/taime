3e696e92b7907f0309b3d80b9e4fabcec19c853d
Nilton Paiva <nfrpaiva@gmail.com>
2019-01-19 18:30:11 -0200

    testes

---
+++ taime-domain/src/test/kotlin/com/nfrpaiva/taime/dominio/test/plano/PlanoTest.kt
@@ -0,0 +1,15 @@
+package com.nfrpaiva.taime.dominio.test.plano
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+class PlanoTest{
+
+    @Test
+    fun insert(){
+
+    }
+
+}
